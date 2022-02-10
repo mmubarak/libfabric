35f2dfc0340d9455c8cd2e889626672f94029752
mmubarak <misbah.mubarak@gmail.com>
2022-02-10 15:43:44 +0000

    fabtests/pytest: updating rdm and rma tests to use message size range
    Updates have been made to pytest/efa tests to use the option
    for message sizes in the range of 8k-9k with an increment of 4.
    
    Signed-off-by: mmubarak <misbah.mubarak@gmail.com>

--- fabtests/pytest/efa/test_rdm.py
+++ fabtests/pytest/efa/test_rdm.py
@@ -10,12 +10,12 @@ from default.test_rdm import test_rdm_atomic
 def memory_type(request):
     return request.param
 
-def run_rdm_test(cmdline_args, executable, iteration_type, completion_type, memory_type):
+def run_rdm_test(cmdline_args, executable, iteration_type, completion_type, memory_type, message_size):
     from common import ClientServerTest
     test = ClientServerTest(cmdline_args, executable, iteration_type,
                             completion_type=completion_type,
                             datacheck_type="with_datacheck",
-                            message_size="all",
+                            message_size=message_size,
                             memory_type=memory_type)
     test.run()
 
@@ -24,18 +24,36 @@ def run_rdm_test(cmdline_args, executable, iteration_type, completion_type, memo
                           pytest.param("standard", marks=pytest.mark.standard)])
 def test_rdm_pingpong(cmdline_args, iteration_type, completion_type, memory_type):
     run_rdm_test(cmdline_args, "fi_rdm_pingpong", iteration_type,
-                 completion_type, memory_type)
+            completion_type, memory_type, "all")
+
+@pytest.mark.parametrize("message_size",
+                         [pytest.param("r:8000,4,9000", marks=pytest.mark.short)])
+def test_rdm_pingpong_range(cmdline_args, completion_type, memory_type, message_size):
+    run_rdm_test(cmdline_args, "fi_rdm_pingpong", "short",
+                 completion_type, memory_type, message_size)
 
 @pytest.mark.parametrize("iteration_type",
                          [pytest.param("short", marks=pytest.mark.short),
                           pytest.param("standard", marks=pytest.mark.standard)])
 def test_rdm_tagged_pingpong(cmdline_args, iteration_type, completion_type, memory_type):
     run_rdm_test(cmdline_args, "fi_rdm_tagged_pingpong", iteration_type,
-                 completion_type, memory_type)
+                 completion_type, memory_type, "all")
+
+@pytest.mark.parametrize("message_size",
+                         [pytest.param("r:8000,4,9000", marks=pytest.mark.short)])
+def test_rdm_tagged_pingpong_range(cmdline_args, completion_type, memory_type, message_size):
+    run_rdm_test(cmdline_args, "fi_rdm_tagged_pingpong", "short",
+                 completion_type, memory_type, message_size)
 
 @pytest.mark.parametrize("iteration_type",
                          [pytest.param("short", marks=pytest.mark.short),
                           pytest.param("standard", marks=pytest.mark.standard)])
 def test_rdm_tagged_bw(cmdline_args, iteration_type, completion_type, memory_type):
     run_rdm_test(cmdline_args, "fi_rdm_tagged_bw", iteration_type,
-                 completion_type, memory_type)
+                 completion_type, memory_type, "all")
+
+@pytest.mark.parametrize("message_size",
+                         [pytest.param("r:8000,4,9000", marks=pytest.mark.short)])
+def test_rdm_tagged_bw_range(cmdline_args, completion_type, memory_type, message_size):
+    run_rdm_test(cmdline_args, "fi_rdm_tagged_bw", "short",
+                 completion_type, memory_type, message_size)
--- fabtests/pytest/efa/test_rma_bw.py
+++ fabtests/pytest/efa/test_rma_bw.py
@@ -11,3 +11,12 @@ def test_rma_bw(cmdline_args, iteration_type, operation_type, completion_type):
     test = ClientServerTest(cmdline_args, command, iteration_type, completion_type, message_size="all")
     test.run()
 
+@pytest.mark.parametrize("operation_type", ["read", "writedata", "write"])
+@pytest.mark.parametrize("message_size",
+                         [pytest.param("r:8000,4,9000", marks=pytest.mark.short)])
+def test_rma_bw_range(cmdline_args, operation_type, completion_type, message_size):
+    from common import ClientServerTest
+    command = "fi_rma_bw -e rdm"
+    command = command + " -o " + operation_type
+    test = ClientServerTest(cmdline_args, command, iteration_type="short", completion_type=completion_type, message_size=message_size)
+    test.run()
